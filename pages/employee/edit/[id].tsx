3df318b38b770a8468a95f9f181ba1cdc2e6cd49
Lihini Galagedara <navo.galagedara@gmail.com>
2022-11-09 14:41:21 +0530

    Code cleanup in edit page

--- pages/employee/edit/[id].tsx
+++ pages/employee/edit/[id].tsx
@@ -1,28 +1,14 @@
-import { useState, useEffect } from 'react'
 import Box from '@mui/material/Box'
-import TextField from '@mui/material/TextField'
-import Grid from '@mui/material/Unstable_Grid2'
-import FormControl from '@mui/material/FormControl'
-import MenuItem from '@mui/material/MenuItem'
-import InputLabel from '@mui/material/InputLabel'
-import Select, { SelectChangeEvent } from '@mui/material/Select'
-import Avatar from '@mui/material/Avatar'
-import Button from '@mui/material/Button'
-import { minWidth } from '@mui/system'
-
 import {
   Employee
 } from '../../../src/types/employeeDataTypes'
 import { useRouter } from 'next/router'
 import { useSelector } from 'react-redux'
 import { selectEmployees } from '../../../src/slices/employee'
-import Typography from '@mui/material/Typography'
 import FormTemplate from '../../../src/components/commons/FormTemplate'
 import BackButton from '../../../src/components/commons/buttons/BackButton'
 
 const EditEmployeeRecord = () => {
-  // const [gender, setGender] = useState('Select gender')
-  // const [fieldError, setFieldError] = useState(false)
 
 const router = useRouter()
 
