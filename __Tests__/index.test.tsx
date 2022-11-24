0b2e35fc8648ce9a559eb1c4743f0b69e22644d3
Lihini Galagedara <navo.galagedara@gmail.com>
2022-11-24 23:55:16 +0530

    Implement test to verify landing page render

--- __Tests__/index.test.tsx
+++ __Tests__/index.test.tsx
@@ -4,11 +4,6 @@ import Home from '@/pages/index'
 describe('Home', () => {
   it('renders a heading', () => {
     render(<Home />)
-    const title = screen.getByText('Welcome to employee management portal')
-    // const heading = screen.getByRole('heading', {
-    //   name: /welcome to next\.js!/i,
-    // })
-
-    // expect(heading).toBeInTheDocument()
+    const title = screen.getByText('Welcome to employee management portal')  
   })
 })
